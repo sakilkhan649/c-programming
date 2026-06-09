39be06552a8fddf96830584823c16a37a3abaa94
mdsak <mdsakilkhan649@gmail.com>
2026-06-10 05:05:48 +0600

    first commit

---
+++ frist_programming.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+/*
+int main(){
+    printf("My Name is Sakil");
+    return 0;
+}
+*/
+/*
+int main(){
+    int score = 100;
+    float gpa = 3.85;
+    char grade = 'A';
+
+    printf("My Score is: %d\n",score);
+    printf("My Gpa is: %f\n",gpa);
+    printf("My Grade is: %c\n",grade);
+
+    return 0;
+}
+*/
+/*
+int main(){
+    int userAge;
+
+    printf("Enter your age: ");
+
+    scanf("%d",&userAge);
+
+    printf("Wow! You are %d years old.\n",userAge);
+
+    return 0;
+}
+*/
+/*
+# include <stdio.h>
+int main(){
+    int num1;
+    int num2;
+    int sum;
+    printf("Enter value of num1 = ");
+    scanf("%d",&num1);
+    printf("Enter value of num2 = ");
+    scanf("%d",&num2);
+    sum=num1 + num2;
+    printf("Result = %d",sum);
+    return 0;
+}
+*/
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    int adition;
+    printf("enter value of a = ");
+    scanf("%d",&a);
+    printf("enter value of b = ");
+    scanf("%d",&b);
+    adition=a-b;
+    printf("Result = %d",adition);
+    return 0;
+}
