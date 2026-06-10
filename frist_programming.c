346388dea8bbd88dc472598c6689e2826fc5e4fc
mdsak <mdsakilkhan649@gmail.com>
2026-06-11 03:37:13 +0600

    add anuther code

--- frist_programming.c
+++ frist_programming.c
@@ -96,15 +96,88 @@ int main(){
     return 0;
 }
 */
+/*
 #include <stdio.h>
 int main(){
     int i;
     for(i=1;i<=10;i++){
         if(i%2==0){
             printf("Even number %d\n",i);
-        }else{
-            printf("Odd number %d\n",i);
+        }
+        //else{
+           // printf("Odd number %d\n",i);
+       // }
+    }
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main(){
+    int x;
+    for(x=1;x<=20;x++){
+        if(x % 3 == 0){
+            printf("%d\n",x);
         }
     }
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main(){
+    int marks[5]={85,90,60,40,70};
+    int i;
+    printf("My friends marks are:\n");
+    for(i = 0;i< 5;i++){
+        printf("Index %d = %d\n",i,marks[i]);
+    }
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main(){
+    int numbers[5]={10,20,30,40,50};
+    int total = 0;
+    int i;
+    for(i=0;i<5;i++){
+           total=total+numbers[i];
+    }
+    printf("Result= %d",total);
+    return 0;
+}
+*/
+/*
+#include<stdio.h>
+int add(int num1,int num2){
+    int result = num1 + num2;
+    return result;
+}
+int main(){
+    int x = 50,y=30;
+    int sum;
+
+    sum = add(x,y);
+
+    printf("The sum is: %d \n",sum);
+
+    return 0;
+}
+*/
+#include <stdio.h>
+int multiply(int num1,int num2){
+    int result = num1 * num2;
+    return result;
+}
+int main(){
+    int n,m;
+    int answer;
+    printf("Enter value of n\n:");
+    scanf("%d",&n);
+    printf("Enter value of m\n:");
+    scanf("%d",&m);
+    answer=multiply(n,m);
+    printf("Result = %d",answer);
     return 0;
 }
