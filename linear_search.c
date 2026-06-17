3880bb6e3c01025af4f3fc8b9a4d592e019badae
mdsak <mdsakilkhan649@gmail.com>
2026-06-18 02:06:59 +0600

    update

--- linear_search.c
+++ linear_search.c
@@ -19,27 +19,29 @@ int main(){
 */
 #include <stdio.h>
 int main(){
+    // বাইনারি সার্চের প্রধান শর্ত: ডাটা ছোট থেকে বড় সাজানো থাকতে হবে
     int arr[] = {10,20,30,40,50,60,70,80,90,100};
-    int key = 70;
+    int key = 70;// আমরা ৭০ কে খুঁজছি
 
     int left = 0;
-    int right = 9;
+    int right = 9;// অ্যারেতে ১০টি ডাটা, তাই শেষের ইনডেক্স ৯
     int mid;
     int found = 0;
 
+    // যতক্ষণ বাম পাশ ডান পাশের চেয়ে ছোট বা সমান থাকবে, লুপ চলবে
     while (left <= right){
-        mid = (left + right) / 2;
+        mid = (left + right) / 2;// মাঝখানের ইনডেক্স বের করলাম
 
-        if (arr[mid] == key){
+        if (arr[mid] == key){// যদি ডাটা মিলে যায়
             printf("Element found at index: %d\n",mid);
             found = 1;
             break;
         }
-        else if (arr[mid]< key){
-            left = mid + 1;
+        else if (arr[mid]< key){// খোঁজার ডাটা যদি মাঝেরটার চেয়ে বড় হয়
+            left = mid + 1;// বাম দিকের অর্ধেক বাদ, তাই left পয়েন্টার এগিয়ে আনলাম
         }
-        else{
-            right = mid - 1;
+        else{// খোঁজার ডাটা যদি মাঝেরটার চেয়ে ছোট হয়
+            right = mid - 1;// ডান দিকের অর্ধেক বাদ, তাই right পয়েন্টার পিছিয়ে দিলাম
         }
     }
     if(found == 0){
